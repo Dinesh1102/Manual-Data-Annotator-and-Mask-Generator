7999bc27e5768f9057affbc65475f4953d3965cc
anuksha11 <118464546+anuksha11@users.noreply.github.com>
2023-04-22 22:33:12 +0530

    Update crop.py

--- routes/crop.py
+++ routes/crop.py
@@ -28,11 +28,15 @@ def crop_image():
         img = request.files['image']
         cropped_img = request.form['croppedImage']
         filename = "static/cropped/"+img.filename
-        cropped_image_decoded = base64.b64decode(cropped_img.split(',')[1])
-        # save the cropped image to a file
-        with open(f'{filename}', 'wb') as f:
-            f.write(cropped_image_decoded)
-    return render_template('crop.html',flag=1,filename=img.filename)
+        if len(cropped_img)>6:
+            cropped_image_decoded = base64.b64decode(cropped_img.split(',')[1])
+            # save the cropped image to a file
+            with open(f'{filename}', 'wb') as f:
+                f.write(cropped_image_decoded)
+            return render_template('crop.html',flag=1,filename=img.filename)
+        else:
+            flash("Image not cropped !!" , "danger")
+            return render_template("crop.html")
 
 #run predictions on cropped image
 def crop_op():
